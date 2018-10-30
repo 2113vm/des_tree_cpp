407837cefdcec88f21c95e84a9868a3d1024c306
googolplex <21138532110q@gmail.com>
2018-10-30 09:28:37 +0500

    Fix depend.make

--- DesTree/DesTree.cpp
+++ DesTree/DesTree.cpp
@@ -3,17 +3,3 @@
 //
 
 #include "DesTree.h"
-
-
-class DesTree {
-
-    int max_depth;
-    int max_leaf_size;
-
-
-    DesTree (int max_depth, int max_leaf_size) {
-        this->max_depth = max_depth;
-        this->max_leaf_size = max_leaf_size;
-    }
-
-};
